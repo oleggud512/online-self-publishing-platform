a49c3e64aad1fdb47a0cdbc0c5a3f0d960448a97
sandgars <oleggud502@gmail.com>
2023-04-16 16:40:15 +0300

    s

--- admin/src/resources/index.ts
+++ admin/src/resources/index.ts
@@ -11,6 +11,7 @@ import { ProfileRepository } from 'features/profiles/data/profile-repository';
 import { CommentRepository } from 'features/comments/data/comment-repository';
 import { PubSub } from 'shared/pub-sub';
 import { ChapterRepository } from 'features/chapters/data/chapter-repository';
+import firebaseConfig from './firebase-config'
 
 
 type Loc = {
@@ -25,16 +26,6 @@ export function configure(config: FrameworkConfiguration): void {
   // config.globalResources([SanitizeHTMLValueConverter]);
   config.globalResources([PLATFORM.moduleName('./elements/loading-indicator')])
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBQx-7ZGkxi08pNR4exenZEcn-zjBCtUco",
-    authDomain: "books-course-work-1.firebaseapp.com",
-    projectId: "books-course-work-1",
-    storageBucket: "books-course-work-1.appspot.com",
-    messagingSenderId: "626801727731",
-    appId: "1:626801727731:web:78e469f28cd128b3f03f86",
-    measurementId: "G-16HR47WC9Z"
-  };
-  
   firebase.initializeApp(firebaseConfig)
 
   const curLocale = localStorage.getItem('curLocale')
@@ -71,26 +62,3 @@ export function configure(config: FrameworkConfiguration): void {
   console.log('init')
 }
 
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBQx-7ZGkxi08pNR4exenZEcn-zjBCtUco",
-  authDomain: "books-course-work-1.firebaseapp.com",
-  projectId: "books-course-work-1",
-  storageBucket: "books-course-work-1.appspot.com",
-  messagingSenderId: "626801727731",
-  appId: "1:626801727731:web:78e469f28cd128b3f03f86",
-  measurementId: "G-16HR47WC9Z"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-*/
